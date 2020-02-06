0b0618403d7cfa3a57737c239ab38c453459b0ca
Wutang445 <austinw556@gmail.com>
2020-02-06 11:52:24 -0500

    first commit

---
+++ storyline.py
@@ -0,0 +1,15 @@
+from introduction import introFunction
+
+# Game Text
+
+print("********************************************");
+print("***** Welcome to the Rockport Limited! *****");
+print("***Are you ready to begin your adventure?***")
+print("********************************************");
+
+introFunction(input())
+
+print("Our story begins as you ride into town, on a wagon with several other rough-looking individuals...")
+print("You don't know any of their names, backgrounds, or where they came from. What do you do? \n")
+
+response = input()
